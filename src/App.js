098df48515099e4198bb48367f7ded6ee7d8a98a
Diya Raj <156573305+D1YaRaJ@users.noreply.github.com>
2024-03-03 15:30:51 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Navbar from './Components/Navbar';
-import Sidebar from './Components/Sidebar';
-import Product from './Components/Product';
-import Product1 from './Components/Product1';
+import axios from 'axios';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [author, setAuthor] = useState('');
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/get-blogs')
+      .then(res => {
+        setBlogs(res.data.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const addNewBlog = () => {
+    axios.post('http://localhost:8080/add-blog', { title, content, author })
+      .then(res => {
+        setBlogs([...blogs, res.data.data]);
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="box"><Sidebar/>
-      <div className="maincontent"><Product/>
-      <div className="maincontent"><Product1/></div>
-      </div>
-      </div>
+    <div className='container'>
+      <h1>Blog App</h1>
+      {
+        blogs.map((blog, key) => (
+          <div key={key} className="card">
+            <h2>{blog.title}</h2>
+            <p>{blog.content}</p>
+            <p>Author: {blog.author}</p>
+          </div>
+        ))
+      }
+      <hr />
+      <label htmlFor="">Title: </label>
+      <input type="text" onChange={(e) => setTitle(e.target.value)} /><br /><br />
+      <label htmlFor=" ">Content: </label>
+      <textarea onChange={(e) => setContent(e.target.value)} /><br /><br />
+      <label htmlFor=" ">Author: </label>
+      <input type="text" onChange={(e) => setAuthor(e.target.value)} /><br /><br />
+      <button onClick={addNewBlog}>Add New Blog</button>
     </div>
   );
 }
